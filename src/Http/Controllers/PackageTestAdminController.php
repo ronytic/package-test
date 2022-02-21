056f0d30b134f7795a737b06a9913432898e9021
Ronald Nina <ronald.nina.layme@gmail.com>
2022-02-21 19:13:09 -0400

    Store Country

--- src/Http/Controllers/PackageTestAdminController.php
+++ src/Http/Controllers/PackageTestAdminController.php
@@ -7,6 +7,7 @@ use ProcessMaker\Http\Resources\ApiCollection;
 use ProcessMaker\Package\PackageTest\Models\Sample;
 use RBAC;
 use Illuminate\Http\Request;
+use ProcessMaker\Package\PackageTest\Models\PackageCountry;
 use URL;
 
 
@@ -14,6 +15,7 @@ class PackageTestAdminController extends Controller
 {
     public function index()
     {
+        // Load page index
         return view('package-test::index-admin');
     }
 
@@ -21,10 +23,10 @@ class PackageTestAdminController extends Controller
     public function searchCountries(Request $request)
     {
         try {
-
+            // Get the region
             $region = $request->input('region', '');
             $curl = curl_init();
-
+            // Get countries from the API
             curl_setopt_array($curl, array(
                 CURLOPT_URL => 'https://api.first.org/data/v1/countries?region=' . $region,
                 CURLOPT_RETURNTRANSFER => true,
@@ -37,8 +39,8 @@ class PackageTestAdminController extends Controller
             ));
 
             $response = curl_exec($curl);
-
             curl_close($curl);
+            // In case of error
             if ($response === false) {
                 throw new \Exception('Error Processing Request', 1);
             }
@@ -62,8 +64,47 @@ class PackageTestAdminController extends Controller
 
     public function store(Request $request)
     {
-        $country = $request->input('country', '');
-        $region = $request->input('region', '');
-        $countryId = $request->input('country_id', '');
+        try {
+            // Get the country
+            $country = $request->input('country', '');
+            $region = $request->input('region', '');
+            $countryId = $request->input('country_id', '');
+            // Search if the country already exists
+            $countries = PackageCountry::searchCountry($countryId);
+            if (count($countries) > 0) {
+                // If the country already exists, return an error
+                throw new \Exception('Country already exists', 1);
+            }
+            // If the country does not exist, create it
+            $dataCountry = [
+                'country' => $country,
+                'region' => $region,
+                'country_id' => $countryId,
+                'status' => 'ENABLED'
+            ];
+            // Save the country
+            $country = PackageCountry::store(new Request($dataCountry));
+            // If not country was created, return an error
+            if (is_null($country)) {
+                throw new \Exception('Error Processing Request', 1);
+            }
+
+            // If the country was saved, return the country
+            $dataResponse = [
+                "status" => "success",
+                'data' => $country,
+                "message" => ""
+            ];
+            // Return the response in json format
+            return response()->json($dataResponse);
+        } catch (\Exception $e) {
+            // If an error occurs, return the error
+            $dataResponse = [
+                "status" => "error",
+                'message' => $e->getMessage()
+            ];
+            // Return the response in json format
+            return response()->json($dataResponse);
+        }
     }
 }
--- src/Models/PackageCountry.php
+++ src/Models/PackageCountry.php
@@ -2,13 +2,36 @@
 
 namespace ProcessMaker\Package\PackageTest\Models;
 
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
 
 class PackageCountry extends Model
 {
-    protected $table = 'sample_skeleton';
+    protected $table = 'package_countries';
 
     protected $fillable = [
-        'id', 'name', 'status'
+        'id', 'country', 'region', 'country_id', 'status'
     ];
+
+    public static function store(Request $request)
+    {
+        $country = new self;
+        $country->country = $request->input('country', '');
+        $country->region = $request->input('region', '');
+        $country->country_id = $request->input('country_id', '');
+        $country->status = $request->input('status', '');
+        $country->save();
+
+        return $country;
+    }
+
+    public static function searchCountry($countryId)
+    {
+        $data = self::where('status', 'ENABLED')
+            ->when($countryId != '', function ($query) use ($countryId) {
+                return $query->where('country_id', $countryId);
+            })
+            ->get();
+        return $data;
+    }
 }
